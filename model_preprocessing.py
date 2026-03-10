f7084c2c438d4b192065a010c74148cde57965fb
Ivan <impocanal@gmail.com>
2026-03-10 19:08:06 +0500

    add preprocessing data

---
+++ model_preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+train_path = "data/train"
+
+data = []
+
+for file in os.listdir(train_path):
+    df = pd.read_csv(os.path.join(train_path, file))
+    data.append(df)
+
+data = pd.concat(data)
+
+X = data[["day_of_week", "price", "advertising"]]
+y = data["sales"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+os.makedirs("models", exist_ok=True)
+
+joblib.dump(scaler, "models/scaler.pkl")
+
+pd.DataFrame(X_scaled).to_csv("data/train/X_train_scaled.csv", index=False)
+y.to_csv("data/train/y_train.csv", index=False)
+
+print("Preprocessing finished")
